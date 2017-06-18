2984ec6fd3027e1395e40d10dbd66f7d0b3fb42b
happyahluwalia <happyahluwalia@gmail.com>
2017-06-18 18:39:32 -0400

    Added easter egg to the Extension :)

--- borderify.js
+++ borderify.js
@@ -1,1 +1,14 @@
 document.body.style.border = "5px solid yellow";
+var secret = 'happy';
+    var keypressed = [];
+    window.addEventListener('keyup', function(e){
+        keypressed.push(e.key);
+
+        // The array would continue to grow, we want to compare the end of the array to our secret
+        keypressed.splice(-secret.length-1,(keypressed.length - secret.length));
+
+        let currentSeq = keypressed.join('');
+        if (secret ===currentSeq){
+            alert('You unlocked an easter egg :)');
+        }
+    })
